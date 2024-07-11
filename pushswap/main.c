6e79e6ebbf7e955ce27e832b140d0d71b0e6ce91
solacode <solacode@solacode>
2024-07-11 05:50:49 -0400

    update

--- pushswap/main.c
+++ pushswap/main.c
@@ -1,20 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#include <string.h>
+// Define the structure of a node in the linked list
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Function to create a new node with given data
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory allocation error\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to append data to the end of the linked list
+void append(Node** head, int data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+
+void is_valid(char *str) {
+    int i = 0;
+    int *list = split_to_int(char *str);
+    while (list[i]) {
+        append();
+        i++;
+    }
+    return 0;
+}
+
+void get_arg(char *str) {
+    if (is_valid(str))
+        return NULL;
+    else {
+
+    }
+}
+
 
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        return printf("Error");
-    char *strtosplit = argv[1];
-    char *token = strtok(strtosplit, " ");
-    while (token!= NULL) {
-        printf("%s\n", token);
-        token = strtok(NULL, " ");
+    if (argc == 1) {
+        printf("number of arguments is not valid \n");
     }
+    else {
+        char *str;
+        int i = 1;
+        while (i < argc) {
+            get_arg(argc[i]);
+            i++;
+        }
 
-    int *b1;
-    int *b2;
+    }
     return 0;
 }
